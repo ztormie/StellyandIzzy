f67cc7d30326e85f7484c43befcbd62cc7611539
ztormie <72881908+ztormie@users.noreply.github.com>
2025-03-09 16:11:06 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,7 @@
-import '../styles/calendar.css';
+import '../styles/calendar.css'; // ✅ Import global styles
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp; // ✅ Ensure there's a default export
